76f85a0e11539759bca84b5106f42678a3bf2678
freiform <sven.kissner@gmail.com>
2019-04-05 19:53:21 +0200

    Update DFMiniMp3.h

--- src/DFMiniMp3.h
+++ src/DFMiniMp3.h
@@ -20,7 +20,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public
-License along with NeoPixel.  If not, see
+License along with DFMiniMp3.  If not, see
 <http://www.gnu.org/licenses/>.
 -------------------------------------------------------------------------*/
 #pragma once
